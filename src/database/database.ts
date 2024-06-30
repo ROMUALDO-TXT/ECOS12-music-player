808c5e14f17acc7b49282733e44d29a388048313
ROMUALDO-TXT <lucasrlemos03@gmail.com>
2024-06-29 23:26:52 -0300

    .env para database

--- src/database/database.ts
+++ src/database/database.ts
@@ -4,12 +4,12 @@ import { User } from '@shared/models/User'
 import { DataSource } from 'typeorm'
 
 export const database = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5460,
-    username: "user",
-    password: "pswd",
-    database: "ecos12",
+    type: 'postgres', 
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT) || 5432,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD, 
+    database: process.env.DB_DATABASE,
     synchronize: true,
     logging: true,
     entities: [Track, User, Playlist],
